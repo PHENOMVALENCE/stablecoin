d23575db620ef4549462e5dd1a00f2eaedcdfcca
PHENOMVALENCE <phenomenalvalence@gmail.com>
2025-09-13 18:46:17 +0300

    clean

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post, FAQ, Event
-
-
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(FAQ)
-admin.site.register(Event)
